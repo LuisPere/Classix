459369a6137a5e0311fe5115167dc6e5c2d073dd
LuisPere <pluis416@gmail.com>
2022-10-10 11:28:54 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,3 +26,19 @@ const sr = ScrollReveal ({
 sr.reveal('.home-text',{delay:280, origin:'bottom'})
 
 sr.reveal('.featured,.cta,.new,.brand,.contact',{delay:200, origin:'bottom'})
+
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+	let messages = []
+	if (name.value === '' || name.value == null) {
+		messages.push('Name is required')
+	}
+	if (messages.length > 0) {
+		e.preventDefault()
+		errorElement.innerText = messages.join(', ')
+	}
+})
